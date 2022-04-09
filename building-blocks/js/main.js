fdd3af62d40f614c6a6763835974930087f7f239
Rick van der Wolk <rickvanderwolk@gmail.com>
2022-04-09 22:33:52 +0200

    Remove shape circle

--- building-blocks/js/main.js
+++ building-blocks/js/main.js
@@ -107,8 +107,8 @@ function hasUnderlyingBlock(block, blocks) {
     let supportBlockY = block.y + block.ySize;
 
     if (block.xSize === (size * 2)) {
-        const foundSupportBlockLeft = getSupportingBlock(blocks, supportBlockX, supportBlockY, ['circle', 'triangle']);
-        const foundSupportBlockRight = getSupportingBlock(blocks, (supportBlockX + size), supportBlockY, ['circle', 'triangle']);
+        const foundSupportBlockLeft = getSupportingBlock(blocks, supportBlockX, supportBlockY, ['triangle']);
+        const foundSupportBlockRight = getSupportingBlock(blocks, (supportBlockX + size), supportBlockY, ['triangle']);
         if (foundSupportBlockLeft && foundSupportBlockRight) {
             return true;
         }
@@ -223,7 +223,6 @@ function getRandomishShape() {
      */
     const shapes = [
         'bridge', 'bridge',
-        'circle', 'circle', 'circle', 'circle',
         'square', 'square', 'square', 'square', 'square', 'square',
         'triangle', 'triangle',
     ];
@@ -240,9 +239,6 @@ function draw() {
         switch (block.shape) {
             case 'bridge':
                 drawBridge(block.x, block.y, block.xSize, block.ySize, block.color);
-                break;
-            case 'circle':
-                drawCircle(block.x, block.y, block.xSize, block.ySize, block.color);
                 break;
             case 'square':
                 drawSquare(block.x, block.y, block.xSize, block.ySize, block.color);

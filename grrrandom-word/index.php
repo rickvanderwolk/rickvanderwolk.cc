bf68db997880df10f019e2a454e0892b23676884
Rick van der Wolk <rickvanderwolk@gmail.com>
2023-03-31 17:55:56 +0200

    Make text not selectable

--- grrrandom-word/index.php
+++ grrrandom-word/index.php
@@ -20,6 +20,11 @@
         h1 {
             font-size: 8em;
             margin: 15px;
+            /* thanks https://stackoverflow.com/questions/6900124/how-to-make-certain-text-not-selectable-with-css */
+            -webkit-user-select: none; /* Safari */
+            -moz-user-select: none; /* Firefox */
+            -ms-user-select: none; /* IE10+/Edge */
+            user-select: none; /* Standard */
         }
         #random-word-wrapper {
             margin-left: auto;

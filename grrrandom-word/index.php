6f2e0418d71a026466feb8b7cbb65c9c29c6d44e
Rick van der Wolk <rickvanderwolk@gmail.com>
2023-03-31 17:58:09 +0200

    Remove unnecessarily id

--- grrrandom-word/index.php
+++ grrrandom-word/index.php
@@ -58,14 +58,13 @@
         $randomWord = $words[array_rand($words)];
     ?>
     <div id="random-word-wrapper">
-        <h1 id="random-word">
-            <?php echo $randomWord; ?>
-        </h1>
+        <h1><?php echo $randomWord; ?></h1>
     </div>
     <script>
         function reloadPage() {
             location.reload();
         }
+
         document.body.addEventListener('click', reloadPage);
         document.body.onkeyup = function(e) {
             if (

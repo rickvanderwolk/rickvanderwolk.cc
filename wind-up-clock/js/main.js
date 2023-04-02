da8c91b8412e36050af4cd16a50060a5517ed931
Rick van der Wolk <rickvanderwolk@gmail.com>
2023-04-02 17:54:39 +0200

    Fix hour and minute hand position
    Use clock time to calculate current hour / minute progress instead of current time

--- wind-up-clock/js/main.js
+++ wind-up-clock/js/main.js
@@ -86,8 +86,8 @@ function draw() {
     // Angles for sin() and cos() start at 3 o'clock;
     // subtract HALF_PI to make them start at the top
     let s = map(getSecond(), 0, 60, 0, TWO_PI) - HALF_PI;
-    let m = map(getMinute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
-    let h = map(getHour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
+    let m = map(getMinute() + norm(getSecond(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
+    let h = map(getHour() + norm(getMinute(), 0, 60), 0, 24, 0, TWO_PI * 2) - HALF_PI;
 
     // Draw the hands of the clock
     stroke(0);

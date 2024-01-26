37769b0fa1c3947d9b2e3e2f65e97a32c655790d
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-01-26 18:45:59 +0100

    Change aliases

--- $/aliases.php
+++ $/aliases.php
@@ -3,6 +3,5 @@
 define('BASE_URL', 'https://www.rickvanderwolk.cc');
 
 $aliases = [
-    'uuid' => BASE_URL . '/uuid',
-    'word' => BASE_URL . '/grrrandom-word',
+    'rw' => BASE_URL . '/grrrandom-word',
 ];

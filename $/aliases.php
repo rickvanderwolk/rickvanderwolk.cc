e1fd882614550e5abc802018d59d44f029305869
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-01-26 22:08:21 +0100

    Add alias for GitHub

--- $/aliases.php
+++ $/aliases.php
@@ -4,6 +4,7 @@ define('BASE_URL', 'https://rickvanderwolk.cc');
 
 $aliases = [
     'cal' => BASE_URL . '/calendar',
+    'gh' => 'https://github.com/rickvanderwolk',
     'pp' => BASE_URL . '/pretty-potato/',
     'rw' => BASE_URL . '/grrrandom-word',
     'uuid' => BASE_URL . '/uuid',

ec4c93eb210dcff910b7105502df0f6ea4035b05
Rick van der Wolk <rickvanderwolk@gmail.com>
2024-05-03 20:18:42 +0200

    Update aliases.php

--- $/aliases.php
+++ $/aliases.php
@@ -3,10 +3,9 @@
 define('BASE_URL', 'https://rickvanderwolk.cc');
 
 $aliases = [
-    'cal' => BASE_URL . '/calendar',
+    'clocks' => BASE_URL . '/#clocks',
     'gh' => 'https://github.com/rickvanderwolk',
     'pp' => BASE_URL . '/pretty-potato/',
-    'rw' => BASE_URL . '/grrrandom-word',
+    'spc' => BASE_URL . '/sun-position-clock/',
     'tc' => BASE_URL . '/html-title-clock',
-    'uuid' => BASE_URL . '/uuid',
 ];
